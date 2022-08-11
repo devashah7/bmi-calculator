7d2365f81768d043a8a57278c02e50d91c1c8334
Deva Shah <devashah12@gmail.com>
2022-08-11 14:48:25 -0400

    neutral

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+//state
+const[weight, setWeight] = useState(0);
+const[height, setHeight] = useState(0);
+const[bmi, setBmi] = useState('');
+const[message, setMessage] = useState('');
+
+let imgSrc = "";
+
   return (
-    <div className="App">
-      Hello World!
+    <div className="app">
+      <div className='container'>
+
+      <h2 className='center' >BMI Calculator</h2>
+      <form>
+      <div>
+        <label>Weight</label>
+        <input value={weight}/>
+      </div>
+      <div>
+        <label>Height</label>
+        <input value={height}/>
+      </div>
+      <div>
+        <button className='btn' type='submit'>Submit</button>
+        <button className='btn btn-outline' type='submit'>Submit</button>
+      </div>
+
+
+      </form>
+      <div className='center'>
+        <h3>your BMI is:{bmi}</h3>
+        <p>{message}</p>
+      </div>
+        <div className='img-container'>
+          <img src={imgSrc} alt=''></img>
+        </div>
+      </div>
     </div>
   );
 }

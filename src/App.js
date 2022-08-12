54752198985855a1aef8b1d524cc51ed87902d46
devashah7 <devashah12@gmail.com>
2022-08-11 22:28:17 -0400

    dellLap

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ let reload =()=>{
       </div>
       <div>
         <button className='btn' type='submit'>Submit</button>
-        <button className='btn btn-outline' onCl type='submit'>Reload</button>
+        <button className='btn btn-outline' onClick={} type='submit'>Reload</button>
       </div>
 
 

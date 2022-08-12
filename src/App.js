7c27b34d7af19a90106d7b26fa47d18afd4e1356
devashah7 <devashah12@gmail.com>
2022-08-11 22:19:50 -0400

    dellLap

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ const[weight, setWeight] = useState(0);
 const[height, setHeight] = useState(0);
 const[bmi, setBmi] = useState('');
 const[message, setMessage] = useState('');
-
 let imgSrc = "";
 
   return (

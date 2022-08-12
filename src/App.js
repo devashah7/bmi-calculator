bf9b4bd7a54bb8fd7063a8f4eb7f33d4a2cc0712
devashah7 <devashah12@gmail.com>
2022-08-11 22:20:17 -0400

    dellLap

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const[height, setHeight] = useState(0);
 const[bmi, setBmi] = useState('');
 const[message, setMessage] = useState('');
 let imgSrc = "";
-let calcBmi =(event) 
+let calcBmi =(event) =
 
   return (
     <div className="app">
